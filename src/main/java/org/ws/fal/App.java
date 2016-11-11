95220cf41013d57310350f575f33a1feb9e77940
jean-ben-guigui <artiduver@gmail.com>
2016-11-11 19:21:06 +0100

    cuc

---
+++ src/main/java/org/ws/fal/App.java
@@ -0,0 +1,13 @@
+package org.ws.fal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello cuc!" );
+    }
+}
